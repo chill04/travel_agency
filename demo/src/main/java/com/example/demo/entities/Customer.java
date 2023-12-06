b11ce17de6bd3dbc0e68103a61554cdc9af60f8c
Admin <chil702@wgu.edu>
2023-12-06 13:19:29 -0800

    prompt D:  Creates attributes for entities, with column names from database tables

--- demo/src/main/java/com/example/demo/entities/Customer.java
+++ demo/src/main/java/com/example/demo/entities/Customer.java
@@ -1,24 +1,40 @@
 package com.example.demo.entities;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+import java.util.Set;
+
 
 @Entity
-@Table(name="customer")
+@Table(name="customers")
 @Getter
 @Setter
 public class Customer {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private  String address;
-    private  String postal_code;
-    private String phone;
-    private Date create_date;
-    private Date last_update;
-    private Division division;
-    private Set<Cart> carts;
+    @Id
+    @Column(name = "customer_id")
+    Long id;
+    @Column(name = "customer_first_name")
+     String firstName;
+    @Column(name = "customer_last_name")
+    String lastName;
+    @Column(name = "address")
+    String address;
+    @Column(name = "postal_code")
+    String postal_code;
+    @Column(name = "phone")
+    String phone;
+    @Column(name = "create_date")
+    Date create_date;
+    @Column(name = "last_update")
+    Date last_update;
+    @Column(name = "division_id")
+    Division division;
+//    @Column("xxxx")
+    Set<Cart> carts;
 }
--- demo/src/main/java/com/example/demo/entities/Division.java
+++ demo/src/main/java/com/example/demo/entities/Division.java
@@ -1,21 +1,35 @@
 package com.example.demo.entities;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+import java.util.Set;
+
 
 @Entity
-@Table(name="customer")
+@Table(name="divisions")
 @Getter
 @Setter
 public class Division {
-    private Long id;
-    private String division_name;
-    private Date create_date;
-    private Date last_update;
-    private Country country;
-    private Long country_ID;
-    private Set<Customer> customers;
+    @Id
+    @Column(name = "division_id")
+    Long id;
+    @Column(name = "division")
+    String division_name;
+    @Column(name = "create_date")
+    Date create_date;
+    @Column(name = "last_update")
+    Date last_update;
+//    @Column("xxxx")
+    Country country;
+    @Column(name = "country_id")
+    Long country_ID;
+//    @Column("xxxx")
+
+    Set<Customer> customers;
 }
