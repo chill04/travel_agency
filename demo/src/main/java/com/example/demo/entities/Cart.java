77270bd16b86aad786d3767ac880aca3d3d6d7b6
Admin <chil702@wgu.edu>
2023-12-09 12:36:43 -0800

    Merges StatusType enum into Cart class (deletes StatusType class file)

--- demo/src/main/java/com/example/demo/entities/Cart.java
+++ demo/src/main/java/com/example/demo/entities/Cart.java
@@ -1,26 +1,36 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
-
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 
 @Entity
-@Table(name="carts")
+@Table(name = "carts")
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Getter
 @Setter
 public class Cart {
+    public enum StatusType {
+        pending,
+        ordered,
+        canceled
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cart_id")
+    @Column(name = "cart_id", nullable = false)
     private Long id;
 
     @Column(name = "order_tracking_number")
@@ -44,10 +54,11 @@ public class Cart {
     @Column(name = "last_update")
     private Date last_update;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    Set<CartItem> cartItem;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "cart")
+    Set<CartItem> cartItem = new HashSet<>();
+
 }
